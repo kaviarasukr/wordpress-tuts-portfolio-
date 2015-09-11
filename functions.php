d85516966db559a15a83219676554873b18d2f16
kaviarasukr <kvrs66@gmail.com>
2015-09-11 06:09:26 +0100

    added archive,excerpt,blog info and featured images functioonalities

--- functions.php
+++ functions.php
@@ -7,11 +7,7 @@ function mywordpress_resources() {
 }
 
 add_action('wp_enqueue_scripts', 'mywordpress_resources');
-// Navigation Menus
-register_nav_menus(array(
-	'primary' => __( 'Primary Menu'),
-	'footer' => __( 'Footer Menu'),
-));
+
 
 // Get top ancestor
 function get_top_ancestor_id() {
@@ -36,3 +32,26 @@ function has_children() {
 	return count($pages);
 	
 }
+// Customize excerpt word count length
+function custom_excerpt_length() {
+	return 25;
+}
+
+add_filter('excerpt_length', 'custom_excerpt_length');
+
+
+// Theme setup
+function mywordpress_setup() {
+	
+	// Navigation Menus
+	register_nav_menus(array(
+		'primary' => __( 'Primary Menu'),
+		'footer' => __( 'Footer Menu'),
+	));
+	
+	// Add featured image support
+	add_theme_support('post-thumbnails');
+	add_image_size('small-thumbnail', 180, 120, true);
+	add_image_size('banner-image', 920, 210, array('left', 'top'));
+}
+add_action('after_setup_theme', 'mywordpress_setup');
